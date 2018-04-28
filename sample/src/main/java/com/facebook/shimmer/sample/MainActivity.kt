a0f52b4d332cfe94453cf7e8ab1b0015bd9dcc0f
sonutiwari <ankorha@gmail.com>
2018-04-28 17:10:21 +0530

    Some changes are for fixes #46 for fixes #45 changes include: 1. Making indentation 4 spaces in place of 2 spaces. 2. changing gradle-wrapper.properties to its default state.

--- sample/src/main/java/com/facebook/shimmer/sample/MainActivity.kt
+++ sample/src/main/java/com/facebook/shimmer/sample/MainActivity.kt
@@ -18,97 +18,110 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import android.widget.ToggleButton
 import com.facebook.shimmer.Shimmer
 import com.facebook.shimmer.ShimmerFrameLayout
 import kotlinx.android.synthetic.main.main.*
 
 class MainActivity : Activity(), View.OnClickListener {
-  private lateinit var shimmerViewContainer: ShimmerFrameLayout
-  private lateinit var presetButtons: Array<Button>
-  private var currentPreset = -1
-  private var toast: Toast? = null
+    private lateinit var shimmerViewContainer: ShimmerFrameLayout
+    private lateinit var presetButtons: Array<Button>
+    private lateinit var toggleButtons: Array<ToggleButton>
+    private var currentPreset = -1
+    private var toast: Toast? = null
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.main)
-    shimmerViewContainer = shimmer_view_container
-    presetButtons = arrayOf(
-        preset_button0,
-        preset_button1,
-        preset_button2,
-        preset_button3,
-        preset_button4
-    )
-    presetButtons.forEach { it.setOnClickListener(this@MainActivity) }
-    selectPreset(0, false)
-  }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+        shimmerViewContainer = shimmer_view_container
+        presetButtons = arrayOf(
+                preset_button0,
+                preset_button1,
+                preset_button2,
+                preset_button3,
+                preset_button4
+        )
+        presetButtons.forEach { it.setOnClickListener(this@MainActivity) }
+        selectPreset(0, false)
 
-  override fun onClick(v: View) {
-    selectPreset(presetButtons.indexOf(v as Button), true)
-  }
-
-  public override fun onResume() {
-    super.onResume()
-    shimmerViewContainer.startShimmer()
-  }
-
-  public override fun onPause() {
-    shimmerViewContainer.stopShimmer()
-    super.onPause()
-  }
-
-  private fun selectPreset(preset: Int, showToast: Boolean) {
-    if (currentPreset == preset) {
-      return
+        toggleButtons = arrayOf(
+                default_toggle_button,
+                slow_and_reverse_toggle_button,
+                thin_straight_and_transparent_toggle_button,
+                sweep_angle_90_toggle_button,
+                spotlight_toggle_button
+        )
+//        toggleButtons.forEach { it.setOnClickListener(this@MainActivity) }
+//        selectPresetToggle(0, false)
     }
 
-    if (currentPreset >= 0) {
-      presetButtons[currentPreset].setBackgroundResource(R.color.preset_button_background)
+    override fun onClick(v: View) {
+        selectPreset(presetButtons.indexOf(v as Button), true)
+//        selectPresetToggle(toggleButtons.indexOf(v as ToggleButton), true)
     }
-    currentPreset = preset
-    presetButtons[currentPreset].setBackgroundResource(R.color.preset_button_background_selected)
 
-    // If a toast is already showing, hide it
-    toast?.cancel()
-
-    val shimmerBuilder = Shimmer.AlphaHighlightBuilder()
-    shimmerViewContainer.setShimmer(
-        when (preset) {
-          1 -> {
-            // Slow and reverse
-            toast = Toast.makeText(this, "Slow and reverse", Toast.LENGTH_SHORT)
-            shimmerBuilder.setDuration(5000L).setRepeatMode(ValueAnimator.REVERSE)
-          }
-          2 -> {
-            // Thin, straight and transparent
-            toast = Toast.makeText(this, "Thin, straight and transparent", Toast.LENGTH_SHORT)
-            shimmerBuilder.setBaseAlpha(0.1f).setDropoff(0.1f).setTilt(0f)
-          }
-          3 -> {
-            // Sweep angle 90
-            toast = Toast.makeText(this, "Sweep angle 90", Toast.LENGTH_SHORT)
-            shimmerBuilder.setDirection(Shimmer.Direction.TOP_TO_BOTTOM)
-
-          }
-          4 -> {
-            // Spotlight
-            toast = Toast.makeText(this, "Spotlight", Toast.LENGTH_SHORT)
-            shimmerBuilder.setBaseAlpha(0f)
-                .setDuration(2000L)
-                .setDropoff(0.1f)
-                .setIntensity(0.35f)
-                .setShape(Shimmer.Shape.RADIAL)
-          }
-          else -> {
-            toast = Toast.makeText(this, "Default", Toast.LENGTH_SHORT)
-            shimmerBuilder
-          }
-        }.build()
-    )
-
-    // Show toast describing the chosen preset, if necessary
-    if (showToast) {
-      toast?.show()
+    public override fun onResume() {
+        super.onResume()
+        shimmerViewContainer.startShimmer()
     }
-  }
+
+    public override fun onPause() {
+        shimmerViewContainer.stopShimmer()
+        super.onPause()
+    }
+
+    private fun selectPreset(preset: Int, showToast: Boolean) {
+        if (currentPreset == preset) {
+            return
+        }
+
+        if (currentPreset >= 0) {
+            presetButtons[currentPreset].setBackgroundResource(R.color.preset_button_background)
+        }
+        currentPreset = preset
+        presetButtons[currentPreset].setBackgroundResource(R.color.preset_button_background_selected)
+
+        // If a toast is already showing, hide it
+        toast?.cancel()
+
+        val shimmerBuilder = Shimmer.AlphaHighlightBuilder()
+        shimmerViewContainer.setShimmer(
+                when (preset) {
+                    1 -> {
+                        // Slow and reverse
+                        toast = Toast.makeText(this, "Slow and reverse", Toast.LENGTH_SHORT)
+                        shimmerBuilder.setDuration(5000L).setRepeatMode(ValueAnimator.REVERSE)
+                    }
+                    2 -> {
+                        // Thin, straight and transparent
+                        toast = Toast.makeText(this, "Thin, straight and transparent", Toast.LENGTH_SHORT)
+                        shimmerBuilder.setBaseAlpha(0.1f).setDropoff(0.1f).setTilt(0f)
+                    }
+                    3 -> {
+                        // Sweep angle 90
+                        toast = Toast.makeText(this, "Sweep angle 90", Toast.LENGTH_SHORT)
+                        shimmerBuilder.setDirection(Shimmer.Direction.TOP_TO_BOTTOM)
+
+                    }
+                    4 -> {
+                        // Spotlight
+                        toast = Toast.makeText(this, "Spotlight", Toast.LENGTH_SHORT)
+                        shimmerBuilder.setBaseAlpha(0f)
+                                .setDuration(2000L)
+                                .setDropoff(0.1f)
+                                .setIntensity(0.35f)
+                                .setShape(Shimmer.Shape.RADIAL)
+                    }
+                    else -> {
+                        toast = Toast.makeText(this, "Default", Toast.LENGTH_SHORT)
+                        shimmerBuilder
+                    }
+                }.build()
+        )
+
+        // Show toast describing the chosen preset, if necessary
+        if (showToast) {
+            toast?.show()
+        }
+    }
 }
